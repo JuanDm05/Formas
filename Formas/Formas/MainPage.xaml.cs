0a1289547288f73ba55b9aaa857cf3b862c14841
FunkierCoder FUNKIER <contactosjuandm@gmail.com>
2024-01-08 09:13:38 -0700

    Agregar archivos de proyecto.

---
+++ Formas/Formas/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Formas
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private  async void Button_Clicked(object sender, EventArgs e)
+        {
+            refre.IsRefreshing = true;
+            Thread.Sleep(1000);
+            refre.IsRefreshing = false;
+
+        }
+    }
+}
